40e9d47749ba5f2a846261285c66e3776c033367
Strahinja Ponjevic <strahinjaponjevic03@gmail.com>
2026-09-09 17:56:20 +0200

    feat(backend): add core user and workout domain entities and enums

---
+++ backend/WorkoutTracker.Domain/Entities/Workout.cs
@@ -0,0 +1,23 @@
+﻿using WorkoutTracker.Domain.Enums;
+
+namespace WorkoutTracker.Domain.Entities
+{
+    public class Workout
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid UserId {  get; set; }
+
+        public ExerciseType ExerciseType { get; set; }
+        public int DurationMinutes { get; set; }
+        public int CaloriesBurned { get; set; }
+
+        public int Difficulty { get; set; }
+        public int Fatigue { get; set; }
+
+        public string? Notes { get; set; }
+        public DateTime WorkoutDate { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public User? User { get; set; }
+    }
+}
---
+++ backend/WorkoutTracker.Domain/Enums/ExerciseType.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkoutTracker.Domain.Enums
+{
+    public enum ExerciseType
+    {
+        Strenght = 1,
+        Cardio = 2,
+        Flexibility = 3,
+        Other = 4
+    }
+}
